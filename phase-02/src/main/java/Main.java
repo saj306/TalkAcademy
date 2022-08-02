4db99c1207a7f9bbd6f39adee686fb91c746624e
saj306 <s.a.jafari306@gmail.com>
2022-08-02 15:27:57 +0430

    asdf

--- phase-02/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("start working ....");
-        JacksonHandller.importDataFromJasonFile();
-        JacksonHandller.exportDataToXmlFile();
-    }
-}
